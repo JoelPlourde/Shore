0278aea350897b00b0f50468eea165f7b3ea2421
joelplourde4 <jplourde@cloudops.com>
2025-01-04 14:43:56 -0500

    Rename Prefabs to "P_" ...

--- Assets/Scripts/Tooling/ConventionTool.cs
+++ Assets/Scripts/Tooling/ConventionTool.cs
@@ -9,13 +9,13 @@ namespace Tools {
         private static readonly string DATA_PREFIX = "D_";
         private static readonly string PREFAB_PREFIX = "P_";
 
+        private static readonly string ASSET_EXTENSION = "*.asset";
+        private static readonly string PREFAB_EXTENSION = "*.prefab";
+
         // Scriptable Objects Suffixes:
         private static readonly string RECIPE_SUFFIX = "_recipe";
         private static readonly string NODE_SUFFIX = "_node";
         private static readonly string DROP_TABLE_SUFFIX = "_drop_table";
-
-        // Prefab Suffixes:
-        private static readonly string ITEM_PREFAB_SUFFIX = " (Object)";
 
         // Scriptable Objects Directories:
         private static readonly string ITEMS_DIRECTORY = "Assets/Resources/Scriptable Objects/Items";
@@ -29,25 +29,25 @@ namespace Tools {
         #region Scriptable Objects
         [MenuItem("Tools/Naming Convention/Scriptable Objects/Rename Items", false, 1)]
         public static void RenameScriptableObjectItems() {
-            RenameFiles(ITEMS_DIRECTORY, DATA_PREFIX);
+            RenameFiles(ITEMS_DIRECTORY, ASSET_EXTENSION, DATA_PREFIX);
             Debug.Log("Rename Items complete!");
         }
 
         [MenuItem("Tools/Naming Convention/Scriptable Objects/Rename Recipes", false, 2)]
         public static void RenameScriptableObjectRecipes() {
-            RenameFiles(RECIPE_DIRECTORY, DATA_PREFIX, RECIPE_SUFFIX);
+            RenameFiles(RECIPE_DIRECTORY, ASSET_EXTENSION, DATA_PREFIX, RECIPE_SUFFIX);
             Debug.Log("Rename Recipes complete!");
         }
 
         [MenuItem("Tools/Naming Convention/Scriptable Objects/Rename Nodes", false, 2)]
         public static void RenameScriptableObjectNodes() {
-            RenameFiles(NODE_DIRECTORY, DATA_PREFIX, NODE_SUFFIX);
+            RenameFiles(NODE_DIRECTORY, ASSET_EXTENSION, DATA_PREFIX, NODE_SUFFIX);
             Debug.Log("Rename Nodes complete!");
         }
 
         [MenuItem("Tools/Naming Convention/Scriptable Objects/Rename Drop Tables", false, 3)]
         public static void RenameScriptableObjectDropTables() {
-            RenameFiles(DROP_TABLE_DIRECTORY, DATA_PREFIX, DROP_TABLE_SUFFIX);
+            RenameFiles(DROP_TABLE_DIRECTORY, ASSET_EXTENSION, DATA_PREFIX, DROP_TABLE_SUFFIX);
             Debug.Log("Rename Drop Tables complete!");
         }
         #endregion
@@ -55,13 +55,13 @@ namespace Tools {
         #region Prefabs
         [MenuItem("Tools/Naming Convention/Prefabs/Rename Items", false, 1)]
         public static void RenamePrefabItems() {
-            RenameFiles(ITEM_PREFAB_DIRECTORY, PREFAB_PREFIX, ITEM_PREFAB_SUFFIX);
+            RenameFiles(ITEM_PREFAB_DIRECTORY, PREFAB_EXTENSION, PREFAB_PREFIX);
             Debug.Log("Rename Items complete!");
         }
         #endregion
 
-        private static void RenameFiles(string directory, string prefix, string suffix = "") {
-            string[] files = Directory.GetFiles(directory, "*.asset", SearchOption.AllDirectories);
+        private static void RenameFiles(string directory, string extension, string prefix, string suffix = "") {
+            string[] files = Directory.GetFiles(directory, extension, SearchOption.AllDirectories);
             foreach (string file in files) {
                 string absolutePath = file.Replace("\\", "/");
                 string fileName = Path.GetFileNameWithoutExtension(file);
@@ -76,6 +76,7 @@ namespace Tools {
         private static string FormatName(string name, string prefix, string suffix) {
             name = name
                 .Replace(prefix, "")
+                .Replace(" (Object)", "")
                 .Replace(" ", "_")
                 .Replace("(", "")
                 .Replace(")", "")
